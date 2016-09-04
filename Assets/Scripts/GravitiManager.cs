6bab40b4d0ce631905bc531ba0d0019b689610b4
OriishiTakahiro <takahiro0914@live.jp>
2016-09-05 00:14:18 +0900

    implemented wall climbing but remain some of bugs

---
+++ Assets/Scripts/GravitiManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GravitiManager : MonoBehaviour {
+
+	[SerializeField]
+		private GameObject cockroach;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider wall) {
+		cockroach.transform.rotation *= Quaternion.Euler(-90, 0, 0);
+	}
+
+}
