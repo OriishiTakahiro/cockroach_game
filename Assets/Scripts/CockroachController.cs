28fbe57ec23fc25775cb4f8a1daae7c302e12727
OriishiTakahiro <takahiro0914@live.jp>
2016-09-04 20:34:46 +0900

    implemented behavior of cockroach

--- Assets/Scripts/CockroachController.cs
+++ Assets/Scripts/CockroachController.cs
@@ -4,13 +4,23 @@ using System.Collections;
 public class CockroachController : MonoBehaviour {
 
 	[SerializeField]
+		private Camera sight;
+	[SerializeField]
 		private float walk_speed;
+
+	private const float CAM_ROT_LIMIT = 80f;
+	private readonly Quaternion INITIAL_ROT = Quaternion.Euler(0, 0, 0);
+
+	private float prev_x_rot;
+	private float prev_y_rot;
 
 	private Rigidbody cockroach_rigid;
 
 	// Use this for initialization
 	void Start () {
 		cockroach_rigid = this.GetComponent<Rigidbody>();
+		prev_x_rot = 0f;
+		prev_y_rot = 0f;
 	}
 	
 	// Update is called once per frame
@@ -27,6 +37,15 @@ public class CockroachController : MonoBehaviour {
 	}
 
 	void rotateSight() {
+		float y_rot = Input.GetAxis("Mouse X");
+		float x_rot = Input.GetAxis("Mouse Y");
+
+		Quaternion tmp_x_rot = sight.transform.rotation * Quaternion.Euler(x_rot * -1, 0, 0);
+		if(CAM_ROT_LIMIT > Quaternion.Angle(INITIAL_ROT, Quaternion.Euler(tmp_x_rot.eulerAngles.x, 0, 0))) {
+			sight.transform.rotation = tmp_x_rot;
+		}
+		transform.rotation *= Quaternion.Euler(0, y_rot, 0);
+
 	}
 
 }
