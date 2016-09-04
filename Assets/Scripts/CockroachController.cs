363ea51d47a75dee8559bd4ac3bc27b7ac88bca5
OriishiTakahiro <takahiro0914@live.jp>
2016-09-04 18:39:16 +0900

    created room

---
+++ Assets/Scripts/CockroachController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CockroachController : MonoBehaviour {
+
+	[SerializeField]
+		private float walk_speed;
+
+	private Rigidbody cockroach_rigid;
+
+	// Use this for initialization
+	void Start () {
+		cockroach_rigid = this.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		rotateSight();
+
+		if(Input.GetKey(KeyCode.W)) {
+			cockroach_rigid.velocity = transform.forward * walk_speed;
+		} else {
+			cockroach_rigid.velocity = Vector3.zero;
+		}
+
+	}
+
+	void rotateSight() {
+	}
+
+}
