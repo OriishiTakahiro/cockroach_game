ae24f69c67cd75a7676199a8f87df8ffed70e826
OriishiTakahiro <takahiro0914@live.jp>
2016-09-07 21:20:43 +0900

    add collider to furnitures

---
+++ Assets/Scripts/GranManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GranManager : MonoBehaviour {
+
+	[SerializeField]
+		private GameObject cockroach;
+
+	private bool on_ground;
+	private CockroachController instance;
+
+	// Use this for initialization
+	void Start () {
+		bool on_ground = true;
+		instance = cockroach.GetComponent<CockroachController>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if(!on_ground) {
+			instance.changeGravityDir(Vector3.up);
+			cockroach.transform.rotation = Quaternion.Euler(0,0,0);
+		}
+		on_ground = false;
+	}
+	void OnTriggerStay(Collider obstacle) {
+		on_ground = true;
+	}
+}
